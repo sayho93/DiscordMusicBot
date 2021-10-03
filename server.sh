1e3a4906e47b03875cdb77272898b8a47954395f
sayho <fishcreek@naver.com>
2021-10-04 01:28:31 +0900

    working on deploy script

--- server.sh
+++ server.sh
@@ -4,19 +4,15 @@ PID_PATH_NAME=/home/opc/server/DiscordMusicBot/server.pid
 COMMAND=(forever --pidFile "$PID_PATH_NAME" start -a -l /dev/null -c ts-node "$SOURCE_DIR")
 
 case $1 in
-    start)
-        echo "Starting $SERVICE_NAME ..."
-        "${COMMAND[@]}"
-        echo "$SERVICE_NAME started..."
+start)
+    echo "Starting $SERVICE_NAME ..."
+    "${COMMAND[@]}"
+    echo "$SERVICE_NAME started..."
     ;;
-    stop)
-        forever stop "$(cat "$PID_PATH_NAME")"
+stop)
+    forever stop "$(cat "$PID_PATH_NAME")"
     ;;
-    restart)
-        forever restart $SOURCE_DIR
+restart)
+    forever restart $SOURCE_DIR
     ;;
 esac
-
-
-
-
