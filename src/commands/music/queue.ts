4605fc2031b6b9998d046ff7826f42e8a333f19e
sayho <fishcreek@naver.com>
2022-06-01 06:36:00 +0900

    dep version update

--- src/commands/music/queue.ts
+++ src/commands/music/queue.ts
@@ -1,6 +1,6 @@
 import {SlashCommandBuilder} from '@discordjs/builders'
 import {Message, MessageEmbed} from 'discord.js'
-import {DiscordBotClient} from '#root/src'
+import {DiscordBotClient, Song} from '#root/src'
 
 export default {
     data: new SlashCommandBuilder().setName('q').setDescription('Stop current playing music'),
@@ -9,7 +9,7 @@ export default {
         const musicData = client.getMusicData()
         if (musicData.queue.length === 1 || musicData.queue.length) return message.reply('Queue is empty')
 
-        const queue: any[] = musicData.queue
+        const queue: Song[] = musicData.queue
         const embed: MessageEmbed = new MessageEmbed().setColor('#ffffff').setTitle('Queue').setThumbnail(queue[1].thumbnail)
 
         queue.forEach((item, idx) => {
