1c4b8633387b153e33ad0a8b7dd07e68fae5d88d
sayho <fishcreek@naver.com>
2022-06-30 01:35:42 +0900

    queue length bug fix

--- src/commands/music/queue.ts
+++ src/commands/music/queue.ts
@@ -7,7 +7,7 @@ export default {
     execute: async (message: Message, client: DiscordBotClient) => {
         if (!message.member?.voice.channel) return message.reply('You have to be in a voice channel to see queue')
         const musicData = client.getMusicData()
-        if (musicData.queue.length === 1 || musicData.queue.length) return message.reply('Queue is empty')
+        if (musicData.queue.length === 1 || !musicData.queue.length) return message.reply('Queue is empty')
 
         const queue: Song[] = musicData.queue
         const embed: MessageEmbed = new MessageEmbed().setColor('#ffffff').setTitle('Queue').setThumbnail(queue[1].thumbnail)
